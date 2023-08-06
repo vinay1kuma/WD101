8033857c46573083f31313d21dce9e9ec0d44080
vinay1kuma <138586574+vinay1kuma@users.noreply.github.com>
2023-08-06 19:12:57 +0530

    uploaded

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("myForm");
+const form = document.getElementById('registrationForm');
+const entriesTable = document.getElementById('entriesTable');
+let entries = [];
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
+function addEntry(name, email, dob, acceptedTerms) {
+    entries.push({ name, email, dob, acceptedTerms });
+    updateTable();
+}
 
-        const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
+function updateTable() {
+    const tableRows = entries.map(entry => `
+        <tr>
+            <td>${entry.name}</td>
+            <td>${entry.email}</td>
+            <td>${entry.dob}</td>
+            <td>${entry.acceptedTerms ? 'Yes' : 'No'}</td>
+        </tr>
+    `).join('');
 
-        if (!validateName(name)) {
-            alert("Please enter a valid name.");
-            return;
-        }
+    entriesTable.innerHTML = `
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Date of Birth</th>
+            <th>Accepted Terms</th>
+        </tr>
+        ${tableRows}
+    `;
+}
 
-        if (!validateEmail(email)) {
-            alert("Please enter a valid email address.");
-            return;
-        }
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const dob = document.getElementById('dob').value;
+    const acceptedTerms = document.getElementById('acceptTerms').checked;
 
-        if (!validatePassword(password)) {
-            alert("Password must be at least 8 characters long.");
-            return;
-        }
+    // Add validation for age (between 18 and 55) and email format here
+    const validAge = calculateAge(dob) >= 18 && calculateAge(dob) <= 55;
+    const validEmail = validateEmail(email);
 
-        alert("Form submitted successfully!");
+    if (validAge && validEmail) {
+        addEntry(name, email, dob, acceptedTerms);
         form.reset();
-    });
-
-    function validateName(name) {
-        return name.trim() !== "";
-    }
-
-    function validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-
-    function validatePassword(password) {
-        return password.length >= 8;
+    } else {
+        alert('Invalid email address or age must be between 18 and 55.');
     }
 });
+
+function calculateAge(dateString) {
+    const today = new Date();
+    const dob = new Date(dateString);
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDifference = today.getMonth() - dob.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+
+    return age;
+}
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
