fc75d528ba903b82e1ba232b6e66a3d3ca56c923
vinay1kuma <138586574+vinay1kuma@users.noreply.github.com>
2023-08-06 21:27:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-const form = document.getElementById('registrationForm');
-const entriesTable = document.getElementById('entriesTable');
-let entries = [];
-
-function addEntry(name, email, dob, acceptedTerms) {
-    entries.push({ name, email, dob, acceptedTerms });
-    updateTable();
-}
-
-function updateTable() {
-    const tableRows = entries.map(entry => `
-        <tr>
-            <td>${entry.name}</td>
-            <td>${entry.email}</td>
-            <td>${entry.dob}</td>
-            <td>${entry.acceptedTerms ? 'Yes' : 'No'}</td>
-        </tr>
-    `).join('');
-
-    entriesTable.innerHTML = `
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Date of Birth</th>
-            <th>Accepted Terms</th>
-        </tr>
-        ${tableRows}
-    `;
-}
-
-form.addEventListener('submit', (event) => {
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const dob = document.getElementById('dob').value;
-    const acceptedTerms = document.getElementById('acceptTerms').checked;
 
-    // Add validation for age (between 18 and 55) and email format here
+    // Get form values
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const dob = document.getElementById("dob").value;
+    const acceptedTerms = document.getElementById("terms").checked;
+
+    // Validate email and age
+    // (Implement your email and age validation logic here)
     const validAge = calculateAge(dob) >= 18 && calculateAge(dob) <= 55;
     const validEmail = validateEmail(email);
 
@@ -45,7 +19,6 @@ form.addEventListener('submit', (event) => {
     } else {
         alert('Invalid email address or age must be between 18 and 55.');
     }
-});
 
 function calculateAge(dateString) {
     const today = new Date();
@@ -64,3 +37,53 @@ function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
+
+
+
+
+
+
+    // Create an object to store the user data
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      dob: dob,
+      acceptedTerms: acceptedTerms
+    };
+
+    // Retrieve existing data from localStorage or initialize an empty array
+    const storedData = JSON.parse(localStorage.getItem("userData")) || [];
+
+    // Add the new user to the data array
+    storedData.push(user);
+
+    // Save the updated data back to localStorage
+    localStorage.setItem("userData", JSON.stringify(storedData));
+
+    // Update the table with the new user entry
+    updateTable(storedData);
+  });
+
+  function updateTable(data) {
+    const tableBody = document.querySelector("#userTable tbody");
+    tableBody.innerHTML = "";
+
+    data.forEach(user => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.password}</td>
+        <td>${user.dob}</td>
+        <td>${user.acceptedTerms ? "Yes" : "No"}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+  }
+
+  // Load the existing data from localStorage on page load
+  const storedData = JSON.parse(localStorage.getItem("userData")) || [];
+  updateTable(storedData);
+  
+ 
